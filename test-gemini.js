59899f7a041e63efc0ea07fcc57d67e04a159bba
Sourav Sarkar <sourav.sarkar.dev.1@gmail.com>
2026-04-28 21:55:58 +0530

    Deploy: test-gemini.js

---
+++ test-gemini.js
@@ -0,0 +1,14 @@
+const key = "AIzaSyBhJAWtw1UIPB0fE9tgATQdb73wetKldSA";
+const model = "gemini-1.5-flash";
+const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${key}`;
+
+fetch(url, {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    contents: [{ role: 'user', parts: [{ text: "Hello" }] }]
+  })
+})
+.then(res => res.json().then(data => ({status: res.status, data})))
+.then(console.log)
+.catch(console.error);
